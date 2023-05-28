2ae6549a53e157035f72a0266e558c020fc5d07b
daki001 <dakicool001@gmail.com>
2023-05-28 18:47:59 +0200

    Solved Day17 Part2

--- Day17/Day17.java
+++ Day17/Day17.java
@@ -14,12 +14,12 @@ public class Day17 {
         long startTime = System.currentTimeMillis();
         int[] input = readFile("src/Day17/testInput");
         System.out.println(part1(input));
-        //System.out.println(part2(input));
+        System.out.println(part2(input));
         System.out.println("Time for Tests: " + (System.currentTimeMillis() - startTime) + " ms\n");
         startTime = System.currentTimeMillis();
         input = readFile("src/Day17/input");
         System.out.println(part1(input));
-        //System.out.println(part2(input));
+        System.out.println(part2(input));
         System.out.println("Time for Solution: " + (System.currentTimeMillis() - startTime) + " ms");
     }
 
@@ -75,7 +75,43 @@ public class Day17 {
     }
 
     public static long part2(int[] input) {
-        return -1;
+        Set<String> velocityPairs = new HashSet<>();
+        int max_y = Math.max(Math.abs(input[2]), Math.abs(input[3]));
+        Map<Integer, List<Integer>> coords = new HashMap<>();
+        for (int i = max_y; i >= -max_y; i--) {
+            int y_coord = 0;
+            int y_velocity = i;
+            int turn = 0;
+            int max_round = 0;
+            while (!(y_coord < Math.min(input[2], input[3]) && y_velocity < 0)) {
+                y_coord += y_velocity;
+                y_velocity--;
+                turn++;
+                if (Math.min(input[2], input[3]) <= y_coord && y_coord <= Math.max(input[2], input[3])) {
+                    if (!coords.containsKey(i)) {
+                        coords.put(i, new ArrayList<>());
+                    }
+                    coords.get(i).add(turn);
+                }
+            }
+        }
+        Iterator<Integer> it = coords.keySet().stream().sorted(Collections.reverseOrder()).iterator();
+        while (it.hasNext()) {
+            int value = it.next();
+            next:
+            for (int v : coords.get(value)) {
+                for (int i = Math.max(input[0], input[1]); i >= 0; i--) {
+                    long distance = horizontalDistance(v, i);
+                    if (distance >= Math.min(input[0], input[1]) && distance <= Math.max(input[0], input[1])) {
+                        velocityPairs.add(i + "," + value);
+                    }
+                    if (distance < Math.min(input[0], input[1])) {
+                        continue next;
+                    }
+                }
+            }
+        }
+        return velocityPairs.size();
     }
 
     public static int[] readFile(String path) {
